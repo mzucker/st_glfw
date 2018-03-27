b98e5319ccc52c72585eb96a661b61157d83b878
Matt Zucker <mzucker1@swarthmore.edu>
2018-03-27 16:41:28 -0400

    use JavaScript keycodes

--- st_glfw.c
+++ st_glfw.c
@@ -1788,24 +1788,124 @@ void cursor_pos_callback(GLFWwindow* window,
 }
 
 //////////////////////////////////////////////////////////////////////
+// thanks, http://keycode.info/
+
+int js_from_glfw_key(int key) {
+
+    if ((key >= 'A' && key <= 'Z') ||
+        (key >= '0' && key <= '9') ||
+        key == ' ') {
+
+        return key;
+
+    }
+
+    switch (key) {
+    case GLFW_KEY_APOSTROPHE: return 222;
+    case GLFW_KEY_COMMA: return 188;
+    case GLFW_KEY_MINUS: return 189;
+    case GLFW_KEY_PERIOD: return 190;
+    case GLFW_KEY_SLASH: return 191;
+    case GLFW_KEY_SEMICOLON: return 186;
+    case GLFW_KEY_EQUAL: return 187;
+    case GLFW_KEY_LEFT_BRACKET: return 219;
+    case GLFW_KEY_BACKSLASH: return 220;
+    case GLFW_KEY_RIGHT_BRACKET: return 221;
+    case GLFW_KEY_GRAVE_ACCENT: return 192;
+    case GLFW_KEY_ESCAPE: return 27;
+    case GLFW_KEY_ENTER: return 13;
+    case GLFW_KEY_TAB: return 9;
+    case GLFW_KEY_BACKSPACE: return 8;
+    case GLFW_KEY_INSERT: return -1;
+    case GLFW_KEY_DELETE: return 8;
+    case GLFW_KEY_RIGHT: return 39;
+    case GLFW_KEY_LEFT: return 37;
+    case GLFW_KEY_DOWN: return 40;
+    case GLFW_KEY_UP: return 38;
+    case GLFW_KEY_PAGE_UP: return -1;
+    case GLFW_KEY_PAGE_DOWN: return -1;
+    case GLFW_KEY_HOME: return -1;
+    case GLFW_KEY_END: return -1;
+    case GLFW_KEY_CAPS_LOCK: return 20;
+    case GLFW_KEY_SCROLL_LOCK: return -1;
+    case GLFW_KEY_NUM_LOCK: return -1;
+    case GLFW_KEY_PRINT_SCREEN: return -1;
+    case GLFW_KEY_PAUSE: return -1;
+    case GLFW_KEY_F1: return 112;
+    case GLFW_KEY_F2: return 113;
+    case GLFW_KEY_F3: return 114;
+    case GLFW_KEY_F4: return 115;
+    case GLFW_KEY_F5: return 116;
+    case GLFW_KEY_F6: return 117;
+    case GLFW_KEY_F7: return 118;
+    case GLFW_KEY_F8: return 119;
+    case GLFW_KEY_F9: return 120;
+    case GLFW_KEY_F10: return 121;
+    case GLFW_KEY_F11: return 122;
+    case GLFW_KEY_F12: return 123;
+    case GLFW_KEY_F13: return -1;
+    case GLFW_KEY_F14: return -1;
+    case GLFW_KEY_F15: return -1;
+    case GLFW_KEY_F16: return -1;
+    case GLFW_KEY_F17: return -1;
+    case GLFW_KEY_F18: return -1;
+    case GLFW_KEY_F19: return -1;
+    case GLFW_KEY_F20: return -1;
+    case GLFW_KEY_F21: return -1;
+    case GLFW_KEY_F22: return -1;
+    case GLFW_KEY_F23: return -1;
+    case GLFW_KEY_F24: return -1;
+    case GLFW_KEY_F25: return -1;
+    case GLFW_KEY_KP_0: return -1;
+    case GLFW_KEY_KP_1: return -1;
+    case GLFW_KEY_KP_2: return -1;
+    case GLFW_KEY_KP_3: return -1;
+    case GLFW_KEY_KP_4: return -1;
+    case GLFW_KEY_KP_5: return -1;
+    case GLFW_KEY_KP_6: return -1;
+    case GLFW_KEY_KP_7: return -1;
+    case GLFW_KEY_KP_8: return -1;
+    case GLFW_KEY_KP_9: return -1;
+    case GLFW_KEY_KP_DECIMAL: return -1;
+    case GLFW_KEY_KP_DIVIDE: return -1;
+    case GLFW_KEY_KP_MULTIPLY: return -1;
+    case GLFW_KEY_KP_SUBTRACT: return -1;
+    case GLFW_KEY_KP_ADD: return -1;
+    case GLFW_KEY_KP_ENTER: return -1;
+    case GLFW_KEY_KP_EQUAL: return -1;
+    case GLFW_KEY_LEFT_SHIFT: return 16;
+    case GLFW_KEY_LEFT_CONTROL: return 17;
+    case GLFW_KEY_LEFT_ALT: return 18;
+    case GLFW_KEY_LEFT_SUPER: return 91;
+    case GLFW_KEY_RIGHT_SHIFT: return 16;
+    case GLFW_KEY_RIGHT_CONTROL: return 17;
+    case GLFW_KEY_RIGHT_ALT: return 18;
+    case GLFW_KEY_RIGHT_SUPER: return 91;
+    case GLFW_KEY_MENU: return -1;
+    default: return -1;
+    }
+    
+}
+
+//////////////////////////////////////////////////////////////////////
 
 void key_callback(GLFWwindow* window, int key,
                   int scancode, int action, int mods) {
 
-    key = toupper(key);
+    int jskey = js_from_glfw_key(key);
 
     if (action == GLFW_PRESS) {
         
-        if (key == GLFW_KEY_ESCAPE) {
+        if (key == 'Q' && (mods & GLFW_MOD_ALT)) {
             
             glfwSetWindowShouldClose(window, GL_TRUE);
             
-        } else if (key == GLFW_KEY_BACKSPACE ||
-                   key == GLFW_KEY_DELETE) {
+        } else if ((key == GLFW_KEY_BACKSPACE || key == GLFW_KEY_DELETE) &&
+                   (mods & GLFW_MOD_ALT)) {
 
             reset();
             
-        } else if (key == '`' || key == '~') {
+        } else if (key == '`' && (mods & GLFW_MOD_ALT)) {
 
             animating = !animating;
             
@@ -1818,35 +1918,35 @@ void key_callback(GLFWwindow* window, int key,
             }
                 
             
-        } else if (key == 'S' && (mods & GLFW_MOD_ALT)) {
+        } else if (toupper(key) == 'S' && (mods & GLFW_MOD_ALT)) {
 
             printf("saving a screenshot!\n");
             single_shot = 1;
 
-        } else if (key >= 0 && key < 256) {
+        } else if (jskey >= 0 && jskey < 256) {
 
-            printf("press %d\n", key);
+            printf("press %d\n", jskey);
 
             for (int c=0; c<3; ++c) {
-                key_press[3*key+c] = 255;
-                if (last_key != key) {
+                key_press[3*jskey+c] = 255;
+                if (last_key != jskey) {
                     key_press[3*last_key+c] = 0;
                 }
-                key_toggle[3*key+c] = ~key_toggle[3*key+c];
-                key_state[3*key+c] = 255;
+                key_toggle[3*jskey+c] = ~key_toggle[3*jskey+c];
+                key_state[3*jskey+c] = 255;
             }
 
-            last_key = key;
+            last_key = jskey;
 
         }
         
-    } else if (action == GLFW_RELEASE && key >= 0 && key < 256) {
+    } else if (action == GLFW_RELEASE && jskey >= 0 && jskey < 256) {
         
-        printf("release %d\n", key);
+        printf("release %d\n", jskey);
         
         for (int c=0; c<3; ++c) {
-            key_state[3*key+c] = 0;
-            key_press[3*key+c] = 0;
+            key_state[3*jskey+c] = 0;
+            key_press[3*jskey+c] = 0;
         }
 
     }
